b7a7c149cf03150cc87e17c00baade1087a9a8a0
SamLangTen <samlangten@outlook.com>
2020-02-11 22:06:58 +0800

    init project

---
+++ JNUnCov2019Checkin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JNUnCov2019Checkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
