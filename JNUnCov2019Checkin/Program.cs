91180c2fae80b2cd1feebc557484f5e791a7cee3
SamLangTen <samlangten@outlook.com>
2020-02-12 15:18:57 +0800

    clear code

--- JNUnCov2019Checkin/Program.cs
+++ JNUnCov2019Checkin/Program.cs
@@ -135,24 +135,6 @@ namespace JNUnCov2019Checkin
                     Console.WriteLine("All Check-in bots have stopped");
                 }
             }
-
-            if (args.Length != 5) return;
-            var cookies = new CookieContainer();
-            var icas = new ICASModule();
-            icas.Cookies = cookies;
-            icas.Login(args[0], args[1]).Wait();
-            Console.WriteLine("Login to ICAS successfully");
-
-            var ehall = new EhallModule();
-            ehall.Cookies = cookies;
-            ehall.Login(args[2]).Wait();
-            Console.WriteLine("Login to EHall successfully");
-
-            var formData = File.ReadAllText(args[3]);
-            var boundsField = File.ReadAllText(args[4]);
-
-            ehall.StudentnCov2019Checkin(boundsField, formData).Wait();
-            Console.WriteLine("nCov2019Checkin successfully");
         }
     }
 }
